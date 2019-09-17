6f6513190453d95600f5f8e818f513f2404e33e1
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-09-17 14:11:14 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -14,7 +14,7 @@ from ask_sdk_core.handler_input import HandlerInput
 
 from ask_sdk_model import Response
 
-from api import SumoAPI
+# from api import SumoAPI
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
