a627a89179331a70e74b4395fc442b7eda4dc909
Roshan Chhetri <rchhetri@sumologic.com>
2019-09-19 13:48:06 +0530

    update scr

--- bin/update_lambda.sh
+++ bin/update_lambda.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 git stash
+git pull origin dev
+git stash apply
 if [ $? -eq 0 ]; then
-    git pull origin dev
-    git stash apply
-    if [ $? -eq 0 ]; then
-        echo "OK"
-        git commit -a -m "updated"
-        ask lambda upload -f arn:aws:lambda:us-east-1:456227676011:function:SumoLogic-12ece74fd46e -s lambda/
-        git push origin dev
-    else
-        echo "FAIL while stashing"
-    fi
+    echo "OK"
+    git commit -a -m "updated"
+    ask lambda upload -f arn:aws:lambda:us-east-1:456227676011:function:SumoLogic-12ece74fd46e -s lambda/
+    git push origin dev
 else
-    echo "no local changes to stash..."
-    git pull origin dev
+    echo "FAIL while stashing"
 fi
-
